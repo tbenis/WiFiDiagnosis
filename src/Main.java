c915c535e5936fe389ad86f1cb6748de043e5a37
Benis Tambe <will.jenifer@yahoo.com>
2021-06-15 14:26:47 -0400

    added in-code comments and implemented the edge case while loop for all questions/steps

--- src/Main.java
+++ src/Main.java
@@ -5,46 +5,73 @@ public class Main {
 
         Scanner scan = new Scanner(System.in);
 
-//        String[] yesArray = new String[]{"yes", "y"};
-//        String[] noArray = new String[]{"no", "n"};
-
-        System.out.println("Reboot the computer ad try to connect");
-        System.out.println("Did that fix it?");
+        //First set of instruction and question
+        System.out.println("Reboot the computer and try to connect");
+        System.out.println("Did that fix the problem?");
+        //Collecting user input
         String response = scan.nextLine();
-
+        //Edge case - In case user neither enters 'yes or no', question persists
+        while (!(response.equals("no") || response.equals("yes"))) {
+            System.out.println("Error! Please enter yes or no");
+            System.out.println("Reboot the computer and try to connect");
+            System.out.println("Did that fix the problem?");
+            response = scan.nextLine();
+        }
+        //instruction and question set #2
         if(response.equals("yes")) {
             System.out.println("Done");
             System.exit(0);
         } else if(response.equals("no")) {
-            System.out.println("Reboot the computer and try to connect");
-            System.out.println("Did that fix it?");
+            System.out.println("Reboot the router and try to connect");
+            System.out.println("Did that fix the problem?");
             response = scan.nextLine();
-        }
 
+            while (!(response.equals("no") || response.equals("yes"))) {
+                System.out.println("Error! Please enter yes or no");
+                System.out.println("Reboot the router and try to connect");
+                System.out.println("Did that fix the problem?");
+                response = scan.nextLine();
+            }
+        }
+        //instruction and question set #3
         if(response.equals("yes")) {
             System.out.println("Done");
             System.exit(0);
         } else if(response.equals("no")) {
             System.out.println("Make sure the cables connecting the router are firmly plugged in and the power is getting to the router");
-            System.out.println("Did that fix it?");
+            System.out.println("Did that fix the problem?");
             response = scan.nextLine();
+            while (!(response.equals("no") || response.equals("yes"))) {
+                System.out.println("Error! Please enter yes or no");
+                System.out.println("Make sure the cables connecting the router are firmly plugged in and the power is getting to the router");
+                System.out.println("Did that fix the problem?");
+                response = scan.nextLine();
+            }
         }
-
+        //instruction and question set #4
         if(response.equals("yes")) {
             System.out.println("Done");
             System.exit(0);
         } else if(response.equals("no")) {
             System.out.println("Move the computer close to the router and try to connect");
-            System.out.println("Did that fix it?");
+            System.out.println("Did that fix the problem?");
             response = scan.nextLine();
+
+            while (!(response.equals("no") || response.equals("yes"))) {
+                System.out.println("Error! Please enter yes or no");
+                System.out.println("Move the computer close to the router and try to connect");
+                System.out.println("Did that fix the problem?");
+                response = scan.nextLine();
+            }
         }
+        //Last instruction #2
         if(response.equals("yes")) {
             System.out.println("Done");
             System.exit(0);
         } else if(response.equals("no")) {
+            //if all responses are 'no', program tells user to contact ISP
             System.out.println("contact your ISP");
         }
         System.exit(0);
-
     }
 }
